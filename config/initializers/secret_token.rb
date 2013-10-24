262ee4e42d3e71a72fb083bf1cf242bf8eef35f0
Matt Scilipoti <matt@scilipoti.name>
2013-10-24 18:14:01 -0400

    rails new db_info -d postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DbInfo::Application.config.secret_key_base = '225ca36da87ac4a1a0d35ff2fad25368ea535c1802b5232594f692fee836527a2f4d8251ab4192b45180aeae7902eb81ba8d9db8daa1a61ebc5fbf1c1375b69e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DbInfo::Application.config.session_store :cookie_store, key: '_db_info_session'
